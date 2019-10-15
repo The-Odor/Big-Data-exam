82f0dee4274c576c5f725c603daa10e949a0601d
The-Odor <theodor.midtbo.alstad@getmail.no>
2019-10-15 11:38:19 +0200

    Added pdf of assignment

--- Task_1/1dStopWords.py
+++ Task_1/1dStopWords.py
@@ -41,7 +41,7 @@ def xmlmapper(source, infile=sys.stdin):
             #mapper_core(words)
 
 
-with open("StopWords.txt","r") as StopWords:
+with open("../StopWords.txt","r") as StopWords:
     StopW = StopWords.readlines()
     StopW = [i[:-1] for i in StopW[:-1]] + [StopW[-1]]
     for i in range(len(StopW)):
